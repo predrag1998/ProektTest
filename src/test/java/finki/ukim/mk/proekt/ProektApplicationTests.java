ccd680387f238ad49957b0ec78d4e28f58d4489a
predrag1998 <101633327+predrag1998@users.noreply.github.com>
2022-03-15 12:27:02 +0100

    Prv

---
+++ src/test/java/finki/ukim/mk/proekt/ProektApplicationTests.java
@@ -0,0 +1,13 @@
+package finki.ukim.mk.proekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
